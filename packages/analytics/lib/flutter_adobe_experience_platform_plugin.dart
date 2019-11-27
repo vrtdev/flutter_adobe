a3970fecfdff845eabcf68c62dbd5eaa40308c3c
Jérémie Vincke <jeremie@movify.com>
2019-11-27 13:47:14 +0100

    Removed useless enum & added documentation

--- packages/analytics/lib/flutter_adobe_experience_platform_plugin.dart
+++ packages/analytics/lib/flutter_adobe_experience_platform_plugin.dart
@@ -4,23 +4,15 @@ import 'package:flutter/services.dart';
 
 const MethodChannel _channel = const MethodChannel('flutter_adobe_experience_platform_plugin');
 
-enum AdobeExtension {
-  Analytics,
-  Campaign,
-  Identity,
-  Lifecycle,
-  Media,
-  Signal,
-  UserProfile,
-}
-
 class AdobeExperiencePlatform {
   AdobeExperiencePlatform._();
 
+  /// Track a new action. Requires ACPCore to be configured and the ACPAnalytics extension to be registered.
   static Future<bool> trackAction(String action, Map<String, String> data) async {
     return await _channel.invokeMethod('track', {'type': 'action', 'key': action, 'data': data});
   }
 
+  /// Track a new state. Requires ACPCore to be configured and the ACPAnalytics extension to be registered.
   static Future<bool> trackState(String state, Map<String, String> data) async {
     return await _channel.invokeMethod('track', {'type': 'action', 'key': state, 'data': data});
   }
