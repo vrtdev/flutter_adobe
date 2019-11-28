d566212a22de61ce87746d204436a04df1a40ca0
Jérémie Vincke <jeremie@movify.com>
2019-11-28 14:22:50 +0100

    Added some documentation

--- packages/adobe_analytics/lib/adobe_analytics.dart
+++ packages/adobe_analytics/lib/adobe_analytics.dart
@@ -5,9 +5,13 @@ import 'package:flutter/services.dart';
 class AdobeAnalytics {
   static const MethodChannel _channel = const MethodChannel('adobe_analytics');
 
+  /// Track a new action. Make sure ACPCore has been configured, and both ACPAnalytics and ACPIdentity
+  /// have been registered before calling this method.
   static Future<bool> trackAction(String action, Map<String, String> contextData) =>
       _channel.invokeMethod('track', {'type': 'action', 'key': action, 'data': contextData});
 
+  /// Track a new state. Make sure ACPCore has been configured, and both ACPAnalytics and ACPIdentity
+  /// have been registered before calling this method.
   static Future<bool> trackState(String action, Map<String, String> contextData) =>
       _channel.invokeMethod('track', {'type': 'state', 'key': action, 'data': contextData});
 }
