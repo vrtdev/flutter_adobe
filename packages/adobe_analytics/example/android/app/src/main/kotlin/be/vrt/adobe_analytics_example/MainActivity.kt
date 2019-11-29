f1a4818de7a5f7ae0a623e0b669beff9e925842b
Jérémie Vincke <jeremie@movify.com>
2019-11-29 13:47:45 +0100

    Added missing Identity extension registration & updated README

--- packages/adobe_analytics/example/android/app/src/main/kotlin/be/vrt/adobe_analytics_example/MainActivity.kt
+++ packages/adobe_analytics/example/android/app/src/main/kotlin/be/vrt/adobe_analytics_example/MainActivity.kt
@@ -2,7 +2,9 @@ package be.vrt.adobe_analytics_example
 
 import android.os.Bundle
 import android.util.Log
+import com.adobe.marketing.mobile.Analytics
 import com.adobe.marketing.mobile.Identity
+import com.adobe.marketing.mobile.LoggingMode
 import com.adobe.marketing.mobile.MobileCore
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
@@ -12,10 +14,12 @@ class MainActivity : FlutterActivity() {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
 
+        MobileCore.setLogLevel(LoggingMode.VERBOSE)
         MobileCore.setApplication(application)
         MobileCore.configureWithAppID("<Your Adobe Environment ID>")
 
         try {
+            Analytics.registerExtension()
             Identity.registerExtension()
         } catch (e: Exception) { // handle exception
             Log.e("Error", e.message)
